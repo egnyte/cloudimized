68e0e31ca8589b1a5ef84b42ed616bf3a0f63109
Kuba Nowacki <jnowacki@egnyte.com>
2024-07-30 15:15:08 +0200

    GCP API call paging limitation - fix

--- cloudimized/gcpcore/gcpquery.py
+++ cloudimized/gcpcore/gcpquery.py
@@ -24,6 +24,9 @@ DEFAULT_NUM_RETRIES = 3
 AGGREGATED_LIST = "aggregatedList"
 DEFAULT_SORT_FIELDS = ["name"]
 
+GCP_PAGE_TOKEN = "pageToken"
+GCP_NEXT_PAGE_TOKEN = "nextPageToken"
+
 class GcpQuery:
     """A class for sending list query to GCP"""
 
@@ -84,6 +87,7 @@ class GcpQuery:
                     query_kwargs[k] = v.replace("<PROJECT_ID>", project_id)
                     logger.debug(f"Replacing <PROJECT_ID> string in param_name: {k}, param_value: {v} "
                                  f"with string {project_id}")
+        result = []
         try:
             # Build function call for resource
             # i.e. run service.projects().list()
@@ -92,15 +96,22 @@ class GcpQuery:
                          f"Query API call: {self.api_call}")
             api_last_call_method = self.api_call.split(".")[-1]
             query_base = reduce(lambda x, y: methodcaller(y)(x), self.api_call.split(".")[:-1], service)
-            logger.debug(f"Query base for API call: {query_base}\nAPI call kwargs: {query_kwargs}")
-            # Run last call on service with arguments if present i.e. (service.projects()).list()
-            request = methodcaller(api_last_call_method, **query_kwargs)(query_base)
-            logger.debug(f"API call request object: {request}")
-            response = request.execute(num_retries=self.num_retries)
-            logger.debug(f"API call response object: {response}")
+            while True:
+                logger.debug(f"Query base for API call: {query_base}\nAPI call kwargs: {query_kwargs}")
+                # Run last call on service with arguments if present i.e. (service.projects()).list()
+                request = methodcaller(api_last_call_method, **query_kwargs)(query_base)
+                logger.debug(f"API call request object: {request}")
+                response = request.execute(num_retries=self.num_retries)
+                logger.debug(f"API call response object: {response}")
+                result.extend(response.get(self.result_items_field, []))
+                #Continue until there is no further page
+                next_page_token = response.get(GCP_NEXT_PAGE_TOKEN, None)
+                if next_page_token is None:
+                    break
+                else:
+                    query_kwargs[GCP_PAGE_TOKEN] = next_page_token
         except Exception as e:
             raise GcpQueryError(f"Issue executing call '{self.api_call}' with args '{self.kwargs}'") from e
-        result = response.get(self.result_items_field, None)
         # Separate handling for aggregatedList call
         if api_last_call_method == AGGREGATED_LIST:
             result = self._parse_aggregated_list(result)
