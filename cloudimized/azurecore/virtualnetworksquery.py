001160043f26b28bf6ba3ac6c4bbf4221d13097e
Kuba Nowacki <jnowacki@egnyte.com>
2024-12-31 15:00:07 +0100

    Initial commit of azurecore

---
+++ cloudimized/azurecore/virtualnetworksquery.py
@@ -0,0 +1,23 @@
+"""
+Azure query for virtual networks
+"""
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.network import NetworkManagementClient
+
+from cloudimized.azurecore.azurequery import AzureQuery
+
+
+class VirtualNetworksQuery(AzureQuery):
+    """
+    Azure query for virtual networks
+    """
+    def _AzureQuery__send_query(self, credential: DefaultAzureCredential, subscription_id: str):
+        """
+        Sends Azure query that lists Virtual Networks in subscription in project.
+        See: https://learn.microsoft.com/en-us/rest/api/virtualnetwork/virtual-networks/list-all?view=rest-virtualnetwork-2024-05-01&tabs=HTTP
+        :param subscription_id: Azure subscription ID to query
+        :return: List of resources that were queried
+        """
+        client = NetworkManagementClient(credential=credential, subscription_id=subscription_id)
+        result = client.virtual_networks.list_all()
+        return result
